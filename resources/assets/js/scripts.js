7115761d4e48738631336e89e69d77549130d112
Josh Evensen <email@joshevensen.com>
2015-10-08 16:32:05 -0500

    add initial assets

---
+++ resources/assets/js/scripts.js
@@ -0,0 +1,7 @@
+//@prepros-append vendor/magnific-popup.js
+//@prepros-append vendor/itemslide.js
+
+//@prepros-append modules/view-port.js
+//@prepros-append modules/smooth-scroll.js
+
+//@prepros-append modules/misc.js
