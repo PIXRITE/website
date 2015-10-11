9ca64a5ec68672bf16259f878bf0df481ba6d05c
Rich Agren <ragren@gmail.com>
2015-10-11 15:49:04 -0600

    Work on the Services Page
    Complete content added including images

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -7,7 +7,7 @@
 <div class="page_top">
     @include('pages._partials.header')
     
-    <h1 class="page_top--heading">Some Short, Catchy Headline</h1>
+    <h1 class="page_top--heading">SERVICES</h1>
     
     @include('pages._partials.page-bar')
 </div>
@@ -15,9 +15,119 @@
 
 <!-- Page Body -->
 <div class="page_body">
-    <div class="page_body--left">Test Content</div>
+    <div class="page_body--left">
+        
+        <h2>We are Web done right.</h2>
+            <aside class="quote">
+                <p class="quote--text">"We're honored with all of the testimonials and referrals you continue to send our way. For nearly 15 years, you have been the reason PIXRITE keeps its doors open.<br> Thank you for keeping it real!"
+                </p>
+                <span class="quote--credit">Jared Rowe, PIXRITE CEO & Creative</span>    
+            </aside>
+        <p >Truth is, not all Web is created equal. Your brother-in-law's best friend from high school can probably set up a site for you overnight. There are companies out there offering Web design for next to nothing—or even free. If you're "lucky," they'll even put a shopping cart together for you—and only charge you a monthly subscription.</p>
+
+        <p>But today's sophisticated online customers demand more ease of use, more engaging content, more innovation than most high school buds can produce. You require detailed analytics to monitor conversion rates, track search trends, manage brand equity and more.</p>
+
+        <p>PIXRITE provides complete Web and interface design, brand building and marketing analysis, search optimization, PPC expert services, and ecommerce that works right and stays fresh.</p>
+        <br><br>
+        <h2>Your satisfaction guaranteed is our promise to you.</h2>
+            <p>Over the last several years, we've learned that nothing pays like making your customers happy. That's why we're giving you our word and commitment, "You'll be taken care of at PIXRITE for however long you decide to stay with us."</p>
+            <p>Should the occasion arise when you have any concerns or reservations about the services we provide, please feel free to let us know immediately and we'll work to help ensure you have reason to prolong your loyalty.</p>
+        
+    </div>
     
-    <div class="page_body--right">Test Content</div>    
+    <div class="page_body--right">
+        <div class="servicelist">
+            <h3><span class="orange">brand & marketing services</span></h3>
+            <h4>investigate imagine ignite influence</h4>
+            <div>
+                <img class="lefthalf" src="img/pano_marketing.jpg">
+                <p class="caption">Brand equity is a valuable asset and the keystone to building a successful website.</p>
+            </div>
+            <h3><span class="orange">web design services</span></h3>
+            <h4>discover design develop deploy deliver</h4>
+             <img class="lefthalf" src="img/pano_website_design.jpg">
+                <p class="caption">We build complete websites. They come with wireframes, storyboards, rich photography, connections to your business systems and audience</p>
+            <h3><span class="orange">online marketing services</span></h3>
+            <h4>audit analyze optimize convert</h4>
+             <img class="lefthalf" src="img/pano_online_marketing.jpg">
+                <p class="caption">We understand search engine marketing, from PPC auditing to conversion rate optimization and everything in between.</p>
+            <h3><span class="orange">interface design services</span></h3>
+            <h4>look feel listen construct design test review launch</h4>
+             <img class="lefthalf" src="img/pano_interface_design.jpg">
+                <p class="caption">You know your customers. We know interface design. Let's create an amazing online experience together.</p>
+        </div>
+        <div class="allservices">
+            <h4><span class="orange">A comprehensive list of our services...</span></h4>
+            <div class="floater">
+               <h5>Utah Web Design Services</h5>
+                <ul>
+                    <li>Custom Web design</li>
+                    <li>Website audit</li>
+                    <li>Online UX analysis</li>
+                    <li>Local Web strategy</li>
+                    <li>Website wireframe</li>
+                    <li>Keyword analysis</li>
+                    <li>Local search marketing</li>
+                    <li>Local search optimization</li>
+                    <li>Conversion rate optimization</li>
+                    <li>Utah conversion rate consulting</li>
+                    <li>PPC auditing</li>
+                    <li>Website build</li>
+                    <li>Flash® animation</li>
+                    <li>Website template manipulation</li>
+                    <li>Utah website hosting</li>
+                    <li>Domain acquisition</li>
+                    <li>Domain monitoring</li>
+                    <li>Domain management</li>
+                </ul>
+            </div>
+            <div class="floater">
+            <h5>Marketing Services</h5>
+               <ul>
+                    <li>Building a brand</li>
+                    <li>Marketing audit</li>
+                    <li>Marketing plan</li>
+                    <li>Internet marketing plan</li>
+                    <li>Corporate style guide</li>
+                    <li>PowerPoint® slide deck</li>
+                    <li>Sales kits</li>
+                    <li>Brochures</li>
+                    <li>Pamphlets</li>
+                    <li>Product packaging</li>
+                    <li>Signs & billboards</li>
+                    <li>Business cards</li>
+                    <li>Catalogs</li>
+                    <li>Clothing</li>
+                    <li>Trade show booths</li>
+                    <li>Magazines</li>
+                    <li>Ad campaigns</li>
+                    <li>Content creation</li>
+                    <li>Integrated marketing<br> communications</li>
+                    <li>Press releases</li>
+                </ul>
+            </div>
+            <div class="floater">
+            <h5>Application Services</h5>
+               <ul>
+                    <li>Widget development</li>
+                    <li>Widget strategy & implementation</li>
+                    <li>Dashboard & control panels</li>
+                    <li>RFP/RFQ automation</li>
+                    <li>Content management systems</li>
+                    <li>Custom application integration</li>
+                    <li>WPF Development</li>
+                    <li>Expression® & Silverlight®</li>
+                    <li>XML translation</li>
+                    <li>Custom adapters</li>
+                    <li>.NET Web 2.0 applications</li>
+                    <li>MSSQL database engineering</li>
+                    <li>Wireframe and prototyping</li>
+                    <li>UX analysis</li>
+                    <li>Focus group studies</li>
+                </ul>
+            </div>
+        </div>    
+    </div>    
 </div>
 
 @endsection
