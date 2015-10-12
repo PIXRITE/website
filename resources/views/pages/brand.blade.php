3ba6747c8aa87d392c05b227dd872ed92fe8b4ee
Rich Agren <ragren@gmail.com>
2015-10-12 12:46:36 -0600

    check in rest of pages

--- resources/views/pages/brand.blade.php
+++ resources/views/pages/brand.blade.php
@@ -10,17 +10,81 @@
 	
 <!-- Page Top -->
 <div class="page_top"> 
-    <h1 class="page_top--heading">Branding Services</h1>
+    <h1 class="page_top--heading">BRAND &amp; MARKETING</h1>
 </div>
 
 
 <!-- Page Body -->
 <div class="page_body">
     <div class="section_inner">
-        <div class="page_body--left">Test Content</div>
+        <div class="page_body--left">  
+            <h2>Help your customers see the brand beyond your logo.</h2>
+                    <aside class="quote">
+                        <p class="quote--text">"Brand is the feeling your customer walks away with, goes to sleep with and remembers from a handshake. It's an entire identity that should permeate every customer interaction."</p>
+                        <span class="quote--credit">Jared Rowe, PIXRITE CEO &amp; Creative </span>    
+                    </aside>
+                <p >Too many businesses sqander one of their most valuable assets. Your brand should be the most monetized asset your company owns. Some businesses can't imagine spending $5000 for a website, let alone a logo. But think for a moment... with a strong mark that embodies the core values of your company, we're talking about the only symbol (with a chance for instant recognition) that you'll have to offer. Before inventory, before facilities, before employees, and yes, even before an awesome website design, comes brand.</p>
 
-        <div class="page_body--right">Test Content</div>
+                <br><br>
+                <h2>Engineer your brand to provide rich ROI and customer satisfaction.</h2>
+                    <p>Your corporate identity should organically reflect your company's mission, vision and values. When brand reflects who you really are in the marketplace, customers naturally place their trust, and their dollars, in your care.</p>
+                    <p>PIXRITE integrates your brand into every communication vehicle we provide for your business. We have new ideas and proven methods for transforming your brand into the asset it can be.</p>
+                <br><br>
+                <h2>Let rich marketing collateral &amp; strategies deliver your message.</h2>
+                <p>Even though we began designing on and for the screen, we have over a decade of experience in the printed design world. We've worked at the press, behind the vinyl sign shops and even within corporate design and marketing teams. If you're looking to enhance the vehicles with which you approach your customers, PIXRITE can give the extra value that extends their shelf life beyond the point of ROI.</p>
+        </div>
+
+
+        <div class="page_body--right">
+             <div class="columns3"> 
+                <div class="floater3">
+                   <h5>Brand &amp; Strategy</h5>
+                   <p>Our creative and marketing teams work together to provide you with a complete brand package. You'll walk away knowing exactly how to present your company to its target audiences.<br><br></p>
+                   <a class="grey_button" href="/contact">get started</a>               
+                </div>
+            
+                <div class="floater3">
+                   <h5>Brand Refresh</h5>
+                   <p>If you haven't quite reached that "tipping point" yet, we can help you refresh your look and feel. It's less-expensive than a rebrand and can still create enough momentum to push both internal and external limits even further!</p>
+                   <a class="grey_button" href="/contact">get started</a>               
+                </div>
+            
+                <div class="floater3">
+                   <h5>Marketing Services</h5>
+                   <p>Looking for some collateral, a billboard or maybe even a commercial? Our creative and production teams can make miracles happen for you just as quickly as you need them.<br><br></p>
+                   <a class="grey_button" href="/contact">get started</a>               
+                </div>
+            </div>    
+            <img class="promoimage" src="../img/brand_full_01.png"> 
+            <div class="columns2">
+               <h5>Utah Brand Design Services</h5>
+               <div class="floater2">
+                    <ul>
+                        <li>Brand visibility</li>
+                        <li>Marketing audit</li>
+                        <li>Marketing plan</li>
+                        <li>Internet marketing plan</li>
+                        <li>Corporate style guide</li>
+                        <li>PowerPoint® slide deck</li>
+                        <li>Sales kits</li>
+                        <li>Brochures</li>
+                        <li>Pamphlet</li>
+                    </ul>
+               </div>
+               <div class="floater2">
+                    <ul>
+                        <li>Product packaging</li>
+                        <li>Signs &amp; billboards</li>
+                        <li>Business cards</li>
+                        <li>Catalogs</li>
+                        <li>Clothing</li>
+                        <li>Trade show booths</li>
+                        <li>Magazines</li>
+                        <li>Ad campaigns</li>
+                    </ul>
+               </div>
+            </div>   
+        </div>
     </div>
-</div>
-
+</div> 
 @endsection
