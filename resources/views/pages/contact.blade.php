698557c77feb944fec38b3fec951334988f98195
Josh Evensen <email@joshevensen.com>
2015-10-23 15:42:04 -0500

    tweaked contact & hire pages

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -25,24 +25,21 @@
 <!-- Page Body -->
 <div class="page_body">
     <div class="section_inner">
-        <div class="page_body--left">
+        <div class="page_body--skinny">
             <h2>Contact Us</h2>
-            
-            <p>Please feel free to call us during business hours to discuss your project. Outside of business hours, please use our contact form for quickest response.</p>
-            
-            <h4>Sales &amp; Support</h4>
-            
-            <p class="contact--hours">Monday-Friday, 9am-5pm PST</p>
-            
-            <p class="contact--phone">{{ $phone }}</p>
-            
-            <p class="contact--email">{{ $email }}</p>
-            
-            <address class="contact--address">{{ $address1 }}<br>{{ $address2 }}</address>
-        </div>
 
-        <div class="page_body--right">
-        
+            <p>Please feel free to call us during business hours to discuss your project. Outside of business hours, please use our contact form for quickest response.</p>
+
+            <h4 class="contact--subheading">Sales &amp; Support</h4>
+
+            <div class="contact--details">
+                <p class="contact--details--main">Monday-Friday, 9am-5pm PST</p>
+                <p>{{ $phone }}</p>
+                <p>{{ $email }}</p>
+            </div>
+
+            <address class="contact--address">{{ $address1 }}<br>{{ $address2 }}</address>
+
             <form class="contact_form js-slidingForm" action="{{ route('contact-form') }}" method="POST">
                 <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
@@ -75,10 +72,6 @@
                     <button class="form--button" type="submit">CONTACT PIXRITE</button>
                 </div>
             </form>
-        </div>
-    
-        <div class="page_body--full">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d44655.188249909275!2d-122.58286954093111!3d45.61166351411913!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54950b0b7da97427%3A0x1c36b9e6f6d18591!2sPortland%2C+OR!5e0!3m2!1sen!2sus!4v1445453397280" width="1200" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div> 
 </div>
--- resources/views/pages/hire.blade.php
+++ resources/views/pages/hire.blade.php
@@ -25,87 +25,89 @@
 <!-- Page Body -->
 <div class="page_body">
     <div class="section_inner">
-        <h2 class="hire_heading">Let's Get Started</h2>
-       
-        <p class="hire_text">Ready to discuss your project with us? You know the drill: please fill out the form below, and we'll get in touch with you shortly. 
-        Too impatient to wait for us to contact you? Give us a call right now at (123) 456-7890.
-        </p>
-        
-        <form class="hire_form js-slidingForm" action="{{ route('hire-form') }}" method="POST">
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-            
-            <!-- Name -->
-            <div class="form_block left">
-                <label class="form--label" for="name">Name</label>
-                <input class="form--input" type="text" name="name" id="name" placeholder="Name">
-            </div>
+        <div class="page_body--skinny">
+            <h2 class="hire_heading">Let's Get Started</h2>
 
-            <!-- Phone Number -->
-            <div class="form_block right">
-                <label class="form--label" for="phone">Phone Number</label>
-                <input class="form--input" type="text" name="phone" id="phone" placeholder="Phone Number">
-            </div>
+            <p class="hire_text">Ready to discuss your project with us? You know the drill: please fill out the form below, and we'll get in touch with you shortly. 
+            Too impatient to wait for us to contact you? Give us a call right now at (123) 456-7890.
+            </p>
 
-            <!-- Email -->
-            <div class="form_block">
-                <label class="form--label" for="email">Email Address</label>
-                <input class="form--input" type="text" name="email" id="email" placeholder="Email Address">
-            </div>
+            <form class="hire_form js-slidingForm" action="{{ route('hire-form') }}" method="POST">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-            <!-- Details -->
-            <div class="form_block">
-                <label class="form--label" for="details">Project Details</label>
-                <textarea class="form--textarea" name="details" id="details" placeholder="Project Details"></textarea>
-            </div>
-            
-            
-            <div class="form_group">
-            
-                <h3 class="form--heading">Optional Details</h3>
-               
-                <!-- Budget -->
+                <!-- Name -->
                 <div class="form_block left">
-                    <label class="form--label" for="budget">Budget</label>
-                    <select class="form--input" type="text" name="budget" id="budget">
-                        <option value="">Budget</option>
-                        <option value="low">&lt; $2,000</option>
-                        <option value="mid_low">$2,000 to $4,000</option>
-                        <option value="mid_high">$4,000 to $8,000</option>
-                        <option value="high">&gt; $8,000</option>
-                    </select>
+                    <label class="form--label" for="name">Name</label>
+                    <input class="form--input" type="text" name="name" id="name" placeholder="Name">
                 </div>
 
-                <!-- Deadline -->
+                <!-- Phone Number -->
                 <div class="form_block right">
-                    <label class="form--label" for="deadline">Deadline</label>
-                    <select class="form--input" type="text" name="deadline" id="deadline">
-                        <option value="">Deadline</option>
-                        <option value="now">Yesterday</option>
-                        <option value="low">1-2 Weeks</option>
-                        <option value="mid-low">3-4 Weeks</option>
-                        <option value="mid-high">1-2 Months</option>
-                        <option value="high">3-4 Months</option>
-                    </select>
+                    <label class="form--label" for="phone">Phone Number</label>
+                    <input class="form--input" type="text" name="phone" id="phone" placeholder="Phone Number">
                 </div>
 
-                <!-- Website -->
-                <div class="form_block left">
-                    <label class="form--label" for="website">Website</label>
-                    <input class="form--input" type="text" name="website" id="website" placeholder="Website">
+                <!-- Email -->
+                <div class="form_block">
+                    <label class="form--label" for="email">Email Address</label>
+                    <input class="form--input" type="text" name="email" id="email" placeholder="Email Address">
                 </div>
 
-                <!-- Hear About Us -->
-                <div class="form_block right">
-                    <label class="form--label" for="hearAbout">How Did You Hear About Us?</label>
-                    <input class="form--input" type="text" name="hearAbout" id="hearAbout" placeholder="How Did You Hear About Us?">
+                <!-- Details -->
+                <div class="form_block">
+                    <label class="form--label" for="details">Project Details</label>
+                    <textarea class="form--textarea" name="details" id="details" placeholder="Project Details"></textarea>
                 </div>
-            </div>
-            
-            <!-- Buttons -->
-            <div class="form_block buttons">
-                <button class="form--button" type="submit">SUBMIT YOUR REQUEST</button>
-            </div>
-        </form>
+
+
+                <div class="form_group">
+
+                    <h3 class="form--heading">Optional Details</h3>
+
+                    <!-- Budget -->
+                    <div class="form_block left">
+                        <label class="form--label" for="budget">Budget</label>
+                        <select class="form--input" type="text" name="budget" id="budget">
+                            <option value="">Budget</option>
+                            <option value="low">&lt; $2,000</option>
+                            <option value="mid_low">$2,000 to $4,000</option>
+                            <option value="mid_high">$4,000 to $8,000</option>
+                            <option value="high">&gt; $8,000</option>
+                        </select>
+                    </div>
+
+                    <!-- Deadline -->
+                    <div class="form_block right">
+                        <label class="form--label" for="deadline">Deadline</label>
+                        <select class="form--input" type="text" name="deadline" id="deadline">
+                            <option value="">Deadline</option>
+                            <option value="now">Yesterday</option>
+                            <option value="low">1-2 Weeks</option>
+                            <option value="mid-low">3-4 Weeks</option>
+                            <option value="mid-high">1-2 Months</option>
+                            <option value="high">3-4 Months</option>
+                        </select>
+                    </div>
+
+                    <!-- Website -->
+                    <div class="form_block left">
+                        <label class="form--label" for="website">Website</label>
+                        <input class="form--input" type="text" name="website" id="website" placeholder="Website">
+                    </div>
+
+                    <!-- Hear About Us -->
+                    <div class="form_block right">
+                        <label class="form--label" for="hearAbout">How Did You Hear About Us?</label>
+                        <input class="form--input" type="text" name="hearAbout" id="hearAbout" placeholder="How Did You Hear About Us?">
+                    </div>
+                </div>
+
+                <!-- Buttons -->
+                <div class="form_block buttons">
+                    <button class="form--button" type="submit">SUBMIT YOUR REQUEST</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
